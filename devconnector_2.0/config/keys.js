b6d7172e5fb2a60f619f82df742f28b78522a38c
NIROSHA.N <59044022+parthinirosha25@users.noreply.github.com>
2021-08-05 18:21:25 +0530

    1

---
+++ devconnector_2.0/config/keys.js
@@ -0,0 +1,5 @@
+{
+    "mongoURI": "mongodb+srv://niro_1:kdpassword@kdclustermongodb.5jyd6.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+    "jwtSecret": "secret";
+    "githubToken": "secret";
+  }
